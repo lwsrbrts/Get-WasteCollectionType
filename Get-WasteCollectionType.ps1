a4c8bd94ccb4d6bb23a11f38c4ad3c65962a9f7e
lwsrbrts <lewis@lewisroberts.com>
2015-11-15 11:54:43 +0000

    Initial commit with code
    Added the three files that make up the core of the solution.

---
+++ Get-WasteCollectionType.ps1
@@ -0,0 +1,114 @@
+﻿#requires -version 4
+
+$ScriptPath = Split-Path $Script:MyInvocation.MyCommand.Path
+Import-Module ".\Send-WasteCollectionInvitations.psm1"
+
+#############
+# VARIABLES #
+#############
+
+# Variables to use when sending the email.
+$Sender = "Waste Collections <bounce@lewisroberts.com>"
+
+# For Send-WasteCollectionInvitations, a hashtable (an associative array) is required.
+# "Name" = display name, "Value" = email address
+$To = @{
+    "Lewis Roberts" = "lewis@lewisroberts.com";
+    "Kym Jones" = "miss.kr.jones@gmail.com";
+}
+# Can be accessed as $To.`Lewis Roberts`
+
+$Subject = "Waste Collection"
+$Server = "smtp.gmail.com"
+$Port = "587"
+
+# To create an encrypted credential used by this script, use this
+# on the machine and logged on as the user that will run the script:
+#Get-Credential | Export-Clixml -Path ($env:COMPUTERNAME+"bounce-lewisroberts.xml")
+$EmailCredentials = Import-Clixml -Path ($env:COMPUTERNAME+"bounce-lewisroberts.xml")
+
+# The house details
+$Postcode = "CW47BN"
+$HouseNo = "9"
+
+##############
+# PROCESSING #
+##############
+
+# Set up a session with the page. ASP.
+$r = Invoke-WebRequest -Uri 'http://online.cheshireeast.gov.uk/MyCollectionDay/' -SessionVariable RequestForm
+
+# Set the form's required information
+$r.Forms.fields['ctl00$ContentPlaceHolder1$txtPostcode'] = $Postcode
+$r.Forms.fields['ctl00$ContentPlaceHolder1$txtHouseNameNum'] = $HouseNo
+$r.Forms.fields['ctl00$ContentPlaceHolder1$btnSearch'] = 'Search'
+
+# POST the form and get the results from the page
+$response = Invoke-WebRequest -Uri 'http://online.cheshireeast.gov.uk/MyCollectionDay/' `
+                              -WebSession $RequestForm `
+                              -Method POST `
+                              -Body $r.forms.Fields `
+                              -ContentType 'application/x-www-form-urlencoded'
+
+# Extract the collectionDates table from the response
+$CollectionDatesTable = ($response.ParsedHtml.getElementsByTagName('table') | Where-Object {$_.className -eq 'collectionDates'}).outerHTML
+
+# Compact the string array to a single string.
+$CollectionDatesTable = $CollectionDatesTable.Replace("`r`n","")
+
+# Clean up the table and convert to CSV
+# All customised depending on the table structure
+# Will break if they change the HTML.
+$CurTable = $CollectionDatesTable `
+                -replace "<THEAD>.*</THEAD>" `
+                -replace "</TR>","`r`n" `
+                -replace "<TR .*`">" `
+                -replace "<TABLE .*<TH>" `
+                -replace "<TD colSpan=5></TD>`r`n" `
+                -replace "</?(TR|TH|B)>" `
+                -replace "</TD><TD>","," `
+                -replace "</?T(D|R)>" `
+                -replace "<(TBODY|THEAD)>" `
+                -replace "</(TBODY|THEAD)>" `
+                -replace "</?(TABLE).*>" 
+$collectionDates = $CurTable | ConvertFrom-Csv -Header Bin,Day,Date,Type | Select-Object Date,Type
+
+# Clean up the table to make sense - convert dates to datetime and change verbose type of collection to "bin colour"
+For ($i = 0; $i -le ($collectionDates.Count-1); $i++) {
+    $collectionDates[$i].Date = Get-Date $collectionDates[$i].Date
+    Switch -Regex ($collectionDates[$i].Type) {
+        "Other" {$collectionDates[$i].Type = "Black Bin"}
+        "Recycling" {$collectionDates[$i].Type = "Silver Bin"}
+        "Garden" {$collectionDates[$i].Type = "Brown Bin"}
+    }
+}
+
+# Select only the dates that occur in the next 6 days (ie. just this week's collections)
+[Array]$Fragment = $collectionDates | Where {$_.Date -le (Get-Date).AddDays(6)} | Select-Object Date,Type
+
+# Now change that date to a format we understand and put the collection bin colour in the subject line.
+For ($i=0;$i -le $Fragment.GetUpperBound(0);$i++) {
+    $Fragment[$i].Date = Get-Date $Fragment[$i].Date -Format "dddd dd/MM/yyyy"
+    If ($i % 2 -eq 1) {
+        $Subject += ' & '+$Fragment[$i].Type
+    }
+    Else {$Subject += ' - '+$Fragment[$i].Type}
+}
+
+# Convert the table to HTML so we can insert it in to our email.
+$Fragment = $Fragment | ConvertTo-Html -Fragment
+
+# Get an HTML template for our email.
+$HtmlTemplate = Get-Content "$ScriptPath\contact_template.htm" -Raw # -Raw parameter gets the file instead of loading each line in an Array.
+    
+# Change the template's placeholders with useful information.
+$HtmlData = $HtmlTemplate.Replace('%%fragment%%', $Fragment)
+
+# Send the email using the template.
+Send-WasteCollectionInvitations -To $To `
+                                -Server $Server `
+                                -Port $Port `
+                                -From $Sender `
+                                -Credentials $EmailCredentials `
+                                -Subject $Subject `
+                                -Body $HtmlData
---
+++ Send-WasteCollectionInvitations.psm1
@@ -0,0 +1,134 @@
+﻿<#
+.Synopsis
+   Sends invitations by email
+.DESCRIPTION
+   Sends invitations by email to a provided hashtable of recipients.
+   You must define the recipients, server, port, credentials, from.
+   The idea is not to run this over and over again for every recipient
+   but to send a single email and include all attendees on the invite.
+.EXAMPLE
+
+   $Recipients = 
+    @{
+        "Lewis Roberts" = "lewis@lewisroberts.com";
+        "Kym Jones" = "miss.kr.jones@gmail.com";
+    }
+    Send-WasteCollectionInvitations -To $Recipients `
+                                    -Server 'smtp.gmail.com' `
+                                    -Port 587 `
+                                    -From 'John Doe <john.doe@gmail.com>' `
+                                    -Credentials $SenderCredentials `
+                                    -Subject 'Waste Collection' `
+                                    -Body $Body
+#>
+Function Send-WasteCollectionInvitations
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [hashtable]$To,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Server,
+
+        [Parameter(Mandatory=$true)]
+        [int]$Port,
+
+        [Parameter(Mandatory=$true)]
+        [string]$From,
+
+        [Parameter(Mandatory=$true)]
+        [pscredential]$Credentials,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Subject,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Body
+
+    )
+    ########
+    # SMTP #
+    ########
+    $Smtp = New-Object System.Net.Mail.SmtpClient
+    $Smtp.Credentials = $Credentials
+
+    $Smtp.Port = $Port
+    $Smtp.Host = $Server
+    $Smtp.EnableSsl = $true
+
+    ########
+    # MAIL #
+    ########
+    $Mail = New-Object System.Net.Mail.MailMessage
+
+    $Mail.From = New-Object System.Net.Mail.MailAddress($From)
+    $Mail.Subject = $Subject
+
+    Foreach ($Recipient in $To.GetEnumerator()) {
+        $Mail.To.Add($($Recipient.Value))
+    }
+
+    ########
+    # BODY #
+    ########
+
+    $TodayUTC = (Get-Date).ToUniversalTime()
+    $Description = ($Subject -replace "Waste Collection - ").ToLower()
+
+    # Add the HTML message body to the email from the injected $Body.
+    $Mail.AlternateViews.Add([Net.Mail.AlternateView]::CreateAlternateViewFromString($Body, 'text/html'))
+    
+    # Build the invitation with StringBuilder
+    $s = New-Object System.Text.StringBuilder
+    [void]$s.AppendLine('BEGIN:VCALENDAR')
+    [void]$s.AppendLine("PRODID:-//lewisroberts//EN")
+    #[void]$s.AppendLine("CALSCALE:GREGORIAN")
+    [void]$s.AppendLine("VERSION:2.0")
+    [void]$s.AppendLine("METHOD:REQUEST")
+    [void]$s.AppendLine("BEGIN:VEVENT")
+    #[void]$s.AppendLine("TRANSP:TRANSPARENT")
+    [void]$s.AppendLine([String]::Format("DTSTART:{0:yyyyMMddT190000Z}", $TodayUTC))
+    [void]$s.AppendLine([String]::Format("DTSTAMP:{0:yyyyMMddTHHmmssZ}", $TodayUTC.AddMinutes(-1)))
+    [void]$s.AppendLine([String]::Format("DTEND:{0:yyyyMMddT200000Z}", $TodayUTC))
+    [void]$s.AppendLine("LOCATION:9 Eastgate Road, Holmes Chapel, Cheshire")
+    [void]$s.AppendLine([String]::Format("UID:{0}", [Guid]::NewGuid()))
+    [void]$s.AppendLine("ORGANIZER;CN=`""+$Mail.From.DisplayName+"`":MAILTO:"+$Mail.From.Address)
+    Foreach ($Recipient in $To.GetEnumerator()) {
+        [void]$s.AppendLine("ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN="+$($Recipient.Name)+":mailto:"+$($Recipient.Value)+"")
+    }
+    #[void]$s.AppendLine("ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=Waste Collections;X-NUM-GUESTS=0:mailto:bounce@lewisroberts.com")
+    [void]$s.AppendLine("SUMMARY;ENCODING=QUOTED-PRINTABLE:$Subject")
+    [void]$s.AppendLine("DESCRIPTION;ENCODING=QUOTED-PRINTABLE:This is a reminder for tomorrow's waste collection. The $Description must be placed at the kerbside by 6am.")
+    [void]$s.AppendLine("SEQUENCE:0")
+    [void]$s.AppendLine("STATUS:CONFIRMED")
+    #[void]$s.AppendLine("PRIORITY:3")
+    #[void]$s.AppendLine("CLASS:PUBLIC")
+    [void]$s.AppendLine("BEGIN:VALARM")
+    [void]$s.AppendLine("TRIGGER;RELATED=START:-PT00H15M00S")
+    [void]$s.AppendLine("ACTION:DISPLAY")
+    [void]$s.AppendLine("DESCRIPTION:Reminder")
+    [void]$s.AppendLine("END:VALARM")
+    [void]$s.AppendLine("END:VEVENT")
+    [void]$s.Append("END:VCALENDAR")
+
+    # Output to disk, if you wish. Outputs to required UTF-8 w/o BOM.
+    #[IO.File]::WriteAllLines('calendarentry.ics', $s.ToString())
+
+    # Create the invitation and add it to the message body.
+    # Doing it this way gives a richer interface in Gmail
+    $ContentType = New-Object System.Net.Mime.ContentType("text/calendar")
+    $ContentType.Parameters.Add("method","REQUEST")
+    $ContentType.Parameters.Add("name", "invite.ics")
+    $Attachment = [Net.Mail.AlternateView]::CreateAlternateViewFromString($s.ToString(), $ContentType)
+    $Mail.AlternateViews.Add($Attachment)
+
+    # Send the email.
+    $Smtp.Send($Mail)
+
+    # Dispose of the resources.
+    $Mail.Dispose()
+    $Smtp.Dispose()
+}
+
+Export-ModuleMember -Function Send-WasteCollectionInvitations
