0efa47161317b195453b719273092184999bf907
lwsrbrts <lewis@lewisroberts.com>
2020-04-29 20:05:15 +0100

    updated to resolve errors parsing dates

--- GetWasteCollectionDay/run.ps1
+++ GetWasteCollectionDay/run.ps1
@@ -1,8 +1,9 @@
 # POST method: $req
+
+
 $requestBody = Get-Content $req -Raw | ConvertFrom-Json
 $Postcode = [System.Web.HttpUtility]::HtmlEncode($requestBody.postcode)
 $HouseNo = [System.Web.HttpUtility]::HtmlEncode($requestBody.houseno)
-
 if ($requestBody.postcode -eq $null -or $requestBody.houseno -eq $null) {
     Out-File -Encoding Ascii -FilePath $res -inputObject '{"error": "Missing postcode or houseno. Both are required."}'
 }
@@ -23,7 +24,7 @@ Catch {
     exit
 }
 
-$searchResult = $search.Links | Where-Object {$_.class -match "get-job-details"}
+$searchResult = $search.Links | Where-Object { $_.class -match "get-job-details" }
 If (-not($searchResult)) {
     Out-File -Encoding Ascii -FilePath $res -inputObject '{"error": "No results were returned or an error occurred. This may indicate that the service or website is unavailable."}'
     exit
@@ -37,32 +38,47 @@ Catch {
     exit
 }
 
-$CollectionDatesTable = $wasteCollections.Content | Select-String -Pattern '<label for=(.|\n|\r)+?<\/label>' -AllMatches | ForEach-Object {$_.Matches} | ForEach-Object {$_.Value}
+$CollectionDatesTable = $wasteCollections.Content | Select-String -Pattern '<label for=(.|\n|\r)+?<\/label>' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
 
 $CurTable = $CollectionDatesTable `
     -replace '<label for="(.)*">' `
     -replace '</label>', ',' `
     -join ''
 
-$Results = $CurTable | Select-String -Pattern '(([^,]*,){3}\s*)' -AllMatches | ForEach-Object {$_.Matches} | ForEach-Object {$_.Value}
+$Results = $CurTable | Select-String -Pattern '(([^,]*,){3}\s*)' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
 
 $collectionDates = ConvertFrom-Csv -Header Day, Date, Type -InputObject $Results
 
 # Clean up the table to make sense - convert dates to datetime objects so we can do calculations
 # on them and change verbose type of collection to "bin colour"
-For ($i = 0; $i -le ($collectionDates.Count - 1); $i++) {
     
-    # If the "WEBSITE_TIME_ZONE" is set to a a region that implements daylight saving, dates change due to midnight being the default
-    # time when a date is converted.
-    # Still thinking about how this should be handled.
-    $collectionDates[$i].Date = [datetime]::ParseExact($collectionDates[$i].Date, "dd/MM/yyyy", [cultureinfo]::InvariantCulture)
+# If the "WEBSITE_TIME_ZONE" is set to a a region that implements daylight saving, dates change due to midnight being the default
+# time when a date is converted.
+# Still thinking about how this should be handled.
 
+$cleanCollectionSchedule = foreach ($Entry in $collectionDates) {
 
-    Switch -Regex ($collectionDates[$i].Type) {
-        "General" {$collectionDates[$i].Type = "Black Bin"}
-        "Recycling" {$collectionDates[$i].Type = "Silver Bin"}
-        "Garden" {$collectionDates[$i].Type = "Brown Bin"}
+    Try {
+        $CollectionDate = [datetime]::ParseExact($Entry.Date, "dd/MM/yyyy", [cultureinfo]::InvariantCulture)
     }
+    Catch {
+        Continue # Skip that entry as we can't decipher the date.
+    }
+
+    Switch -Regex ($Entry.Type) {
+        "General" { $Type = "Black Bin" }
+        "Recycling" { $Type = "Silver Bin" }
+        "Garden" { $Type = "Brown Bin" }
+    }
+
+    $Property = [ordered]@{
+        Day  = $Entry.Day
+        Date = $CollectionDate
+        Type = $Type
+
+    }
+
+    New-Object -TypeName PSObject -Property $Property
 }
 
-Out-File -Encoding Ascii -FilePath $res -inputObject ($collectionDates | ConvertTo-Json -Depth 5)
+Out-File -Encoding Ascii -FilePath $res -inputObject ($cleanCollectionSchedule | ConvertTo-Json -Depth 5)
